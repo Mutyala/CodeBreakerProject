0fdc8ab1fe5ff317136534175ac0f14489f92138
Sindhuri <sindhuri@Sindhuris-MacBook-Pro.local>
2017-09-08 16:18:06 -0700

    Corrected the functions as per live demo

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,81 +1,84 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
-let input = document.getElementById('user-guess');
+
 
 function guess() {
+  let input = document.getElementById('user-guess');
+  //add functionality to guess function here
+  if (answer.value == '' && attempt.value == '') {
+    setHiddenFields();
+  }
+  if (validateInput(input.value) == false) {
+    return false;
+  }
+  attempt.value++;
 
-    //add functionality to guess function here
-    if(answer==''&&attempt==''){
-      setHiddenFields();
-    }
-    if(validateInput(input.value)==false)
-    {
-      return false;
-    }
-    else{
-      attempt++;
-    }
-    getResults(String input);
-    if(true){
-      setMessage("You Win! :)");
-      showAnswer(true);
-      showReplay();
-    }
-    else if(false&&attempt>=10){
-      setMessage("You Lose! :(");
-      showAnswer(false);
-      showReplay();
-    }
-    else {
-      setMessage("Incorrect, try again.");
-    }
+  if (getResults(input.value)) {
+    setMessage("You Win! :)");
+    showAnswer(true);
+    showReplay();
+  } else if (attempt >= 10) {
+    setMessage("You Lose! :(");
+    showAnswer(false);
+    showReplay();
+  } else {
+    setMessage("Incorrect, try again.");
+  }
 }
 
 //implement new functions here
-function setHiddenFields(){
-  attempt=0;
-  answer=Math.random(10000);
-  var input=answer.toString();
-  while(input.length()<4)
-  {
-    input="0"+input;
+function setHiddenFields() {
+  attempt.value = 0;
+  answer.value = Math.floor(Math.random() * 10000).toString();
+  while (answer.value.length < 4) {
+    answer.value = "0" + answer.value;
   }
 }
 
-function setMessage(String message){
-  console.log(message);
+function setMessage(message) {
+  document.getElementById('message').innerHTML = message;
 }
-function validateInput(String input){
-  if(input.length()==4){
+
+function validateInput(input) {
+  if (input.length == 4) {
     return true;
-  }
-  else{
+  } else {
     setMessage("Guesses must be exactly 4 characters long.");
     return false;
   }
 }
 
-function getResults(String results){
-  var initial_div=<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">;
-  foreach(var i : result){
-    initial_div+=<span class="glyphicon glyphicon-ok"></span><span class="glyphicon glyphicon-transfer"></span>
-    <span class="glyphicon glyphicon-remove"></span>;
+function getResults(input) {
+  let initial_div = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+  for (int i = 0; i < input.length; i++) {
+    if (input.charAt(i) == answer.value.charAt(i)) {
+      initial_div += '<span class="glyphicon glyphicon-ok">';
+    } else if (answer.value.indexOf(input.charAt(i)) > -1) {
+      initial_div += '</span><span class="glyphicon glyphicon-transfer"></span>';
+    } else {
+      initial_div += '<span class="glyphicon glyphicon-remove"></span>';
+    }
   }
-  </div>
-  results.innerHTML=initial_div;
-  var characters;
+  initial_div += < /div></div >
+    document.getElementById('results').innerHTML += initial_div;
+  if (input == answer.value) {
+    return true;
+  }
+  return false;
 }
-function showAnswer(boolean val){
-  if(val==true){
-    code.innerHTML=answer.value;
-    code.className=success;
-  }
-  else {
-    code.className=failure;
+
+function showAnswer(val) {
+  let code = document.getElementById('code');
+  code.innerHTML = answer.value;
+  if (val) {
+    code.className += ' success';
+  } else {
+    code.className += ' failure';
   }
 
 }
-function showReplay(){
-  style.display('guessing-div')=none;
-  style.display('replay-div')=block;
+
+function showReplay() {
+  document.getElementById('guessing-div').style.display = 'none';
+  document.getElementById('replay-div').style.display = 'block';
 }
